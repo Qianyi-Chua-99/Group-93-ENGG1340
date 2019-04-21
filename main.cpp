e09099f147f0efc8abeb223a3df6bbec2b2f87ca
limlechi <lechilim@connect.hku.hk>
2019-04-21 15:46:03 +0800

    adding Enter shop and create shop functions

--- main.cpp
+++ main.cpp
@@ -11,8 +11,9 @@ int main() {
 	//initialize everything
 	Shop::readData("Information.txt");
 
-	//declare function for Enter a Shop
-	void 
+	void EnterShop();
+	void CreateShop();
+	void Inventory_Options();
 	
 	//Welcome message
 	cout << "******************************" << endl;
@@ -29,43 +30,26 @@ int main() {
 		{
 		case '1':
 			//Enter shop options function
-			ChooseShop();
+			EnterShop();
 			break;
 		case '2':
 			//Create shop options fucntion
 			CreateShop()
 			break;
+		default:
+			cout << "Option Invalid! Please choose again: " << endl;
 		}
 		
 	}
 	
 
-
-/*
-//Welcome message
-void displayWelcome() {
-	cout << setfill('*') << setw(30) << endl;
-	cout << endl;
-	cout << " Welcome to Inventory System! " << endl;
-	cout << endl;
-	cout << setfill('*') << setw(30) << endl;
-}
-
-//Choose store
-void displayShop() {
-	int i;
-	cout << endl;
-	cout << setfill('*') << setw(30) << endl;
-	cout << "Please select a store" << endl;
-	// for ( i = 0; i < VECTOR_SHOP??.size() ; i ++)
-		//cout << "		" << i << '.' << " " << Shop[i] << endl;
-	cout << "		" << (i+1) << '.' << " " << "Create New Shop" << endl;
-	cout << endl;
-	cout << setfill('*') << setw(30) << endl;
-	cout << endl;
-*/
-}
-
-
-
-
+	void EnterShop() {
+		/*print all shop names
+		if valid option, continue on with inventory choices*/
+		Inventory_Options();
+		//else
+		cout << "Option Invalid! Please choose again: " << endl;
+	}
+	
+	void CreateShop() {
+	}
